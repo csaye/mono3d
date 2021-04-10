e2572ab79c49df5f2886ffcbce0605b4720ecd16
Cooper Saye <coopersaye@gmail.com>
2021-04-10 09:40:13 -0600

    Implement player movement.

--- Test3D/Map.cs
+++ Test3D/Map.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Test3D
 {
@@ -9,6 +11,15 @@ namespace Test3D
         private const int width = 32;
         private const int height = 32;
         private const int length = 32;
+
+        private Vector3 position;
+        private Vector2 angle;
+
+        private Vector2 direction;
+        private Vector2 rotation;
+
+        private const float Speed = 1;
+        private const float Spin = 1;
 
         public Map()
         {
@@ -34,6 +45,53 @@ namespace Test3D
                     }
                 }
             }
+
+            // Initialize player
+            position = new Vector3(width / 2, height / 2, length / 2);
+            angle = new Vector2(0, 0);
+        }
+
+        public void Update(GameTime gameTime, Game1 game)
+        {
+            float delta = (float)gameTime.ElapsedGameTime.TotalSeconds; // Get time delta
+            ProcessKeyboardState(game); // Process keyboard state
+            MovePlayer(delta); // Move player by delta
+        }
+
+        public void Draw(Game1 game)
+        {
+
+        }
+
+        private void ProcessKeyboardState(Game1 game)
+        {
+            KeyboardState state = game.KeyboardState;
+
+            // Get movement direction
+            if (state.IsKeyDown(Keys.W)) direction.Y = 1;
+            else if (state.IsKeyDown(Keys.S)) direction.Y = -1;
+            else direction.Y = 0;
+            if (state.IsKeyDown(Keys.D)) direction.X = 1;
+            else if (state.IsKeyDown(Keys.A)) direction.X = -1;
+            else direction.X = 0;
+
+            // Get movement rotation
+            if (state.IsKeyDown(Keys.Up)) rotation.Y = 1;
+            else if (state.IsKeyDown(Keys.Down)) rotation.Y = -1;
+            else rotation.Y = 0;
+            if (state.IsKeyDown(Keys.Right)) rotation.X = 1;
+            else if (state.IsKeyDown(Keys.Left)) rotation.X = -1;
+            else rotation.X = 0;
+        }
+
+        // Moves player based on given delta
+        private void MovePlayer(float delta)
+        {
+            // Update angle
+            angle += rotation * Spin * delta;
+
+            // Update position
+            position += new Vector3(direction.X, direction.Y, 0) * Speed * delta;
         }
     }
 }
